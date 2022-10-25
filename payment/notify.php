c49400c3a30333a734041c30e2824a2649f7cbb8
Nayab Ali <nayab8609@gmail.com>
2022-10-25 16:48:53 +0500

    notify.php update

--- payment/notify.php
+++ payment/notify.php
@@ -49,84 +49,9 @@ try {
 	echo "Message could not be sent. Mailer Error";
 }
 
-// foreach ($raw_post_array as $keyval) {
-// 	$keyval = explode('=', $keyval);
-// 	if (count($keyval) == 2)
-// 		$myPost[$keyval[0]] = urldecode($keyval[1]);
-// }
-
-// Build the body of the verification post request, 
-// adding the _notify-validate command.
-// $req = 'cmd=_notify-validate';
-// if (function_exists($get_magic_quotes_gpc)) {
-// 	$get_magic_quotes_exists = true;
-// }
-// foreach ($myPost as $key => $value) {
-// 	if ($get_magic_quotes_exists == true && $get_magic_quotes_gpc() == 1) {
-// 		$value = urlencode(stripslashes($value));
-// 	} else {
-// 		$value = urlencode($value);
-// 	}
-// 	$req .= "&$key=$value";
-// }
-
-
-/*
-Post IPN data back to PayPal using curl to 
-validate the IPN data is valid & genuine
-Anyone can fake IPN data, if you skip it.
-*/
-// $ch = curl_init(PAYPAL_URL);
-// if ($ch == FALSE) {
-// 	return FALSE;
-// }
-
-// curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-// curl_setopt($ch, CURLOPT_POST, 1);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-// curl_setopt($ch, CURLOPT_SSLVERSION, 6);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-// curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-
-
-/*
-This is often required if the server is missing a global cert
-bundle, or is using an outdated one.
-Please download the latest 'cacert.pem' from 
-http://curl.haxx.se/docs/caextract.html
-*/
-// if (LOCAL_CERTIFICATE == TRUE) {
-// 	curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . "/cert/cacert.pem");
-// }
-
-// Set TCP timeout to 30 seconds
-// curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-// 	'Connection: Close',
-// 	'User-Agent: PHP-IPN-Verification-Script'
-// ));
-
-// $res = curl_exec($ch);
 
 
 
-// cURL error
-// if (curl_errno($ch) != 0) {
-// 	curl_close($ch);
-// 	exit;
-// } else {
-// 	curl_close($ch);
-// }
-
-
-/* 
- * Inspect IPN validation result and act accordingly 
- * Split response headers and payload, a better way for strcmp 
- */
-// $tokens = explode("\r\n\r\n", trim($res));
-// $res = trim(end($tokens));
 
 
 
@@ -167,8 +92,5 @@ if (!empty($unique_txn_id)) {
 	$db->bind(":currency", $currency);
 	$db->bind(":txn_id", $txn_id);
 	$db->execute();
-	/* error_log(date('[Y-m-d H:i e] '). 
-		"Verified IPN: $req ". PHP_EOL, 3, IPN_LOG_FILE);
-		*/
 }
 $db->close();
